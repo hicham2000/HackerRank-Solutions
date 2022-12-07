52eb7176514cdfc2d1633dd8974534f0c2a68846
hicham taib <oo906496@gmail.com>
2022-12-07 11:19:32 +0100

    Java_Static_Initializer_Block

---
+++ src/Java_Static_Initializer_Block.java
@@ -0,0 +1,33 @@
+import java.io.*;
+import java.util.*;
+import java.text.*;
+import java.math.*;
+import java.util.regex.*;
+
+public class Java_Static_Initializer_Block {
+    Scanner scanner= new Scanner(System.in);
+    public static int B ;
+    public static int H;
+    public static boolean flag;
+
+    static{
+        Scanner scanner = new Scanner(System.in);
+        B = scanner.nextInt();
+        H = scanner.nextInt();
+
+        if(B>0 && H>0){
+            flag = true;
+        }
+        else{
+            System.out.println("java.lang.Exception: Breadth and height must be positive");
+        }
+    }
+
+    public static void main(String[] args){
+        if(flag){
+            int area=B*H;
+            System.out.print(area);
+        }
+
+    }//end
+}
