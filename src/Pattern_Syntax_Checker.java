65ea9eee29737cb88be58b414187ef6e51e0bd71
hicham taib <oo906496@gmail.com>
2022-12-09 22:28:21 +0100

    Pattern Syntax Checker

---
+++ src/Pattern_Syntax_Checker.java
@@ -0,0 +1,23 @@
+import java.util.Scanner;
+import java.util.regex.*;
+
+public class Pattern_Syntax_Checker {
+    public static void main(String[] args){
+        Scanner in = new Scanner(System.in);
+        int testCases = Integer.parseInt(in.nextLine());
+        while(testCases>0){
+            String pattern = in.nextLine();
+            //Write your code
+            String test = "test";
+            try{
+                test.matches(pattern);
+                System.out.println("Valid");
+            }
+            catch(Exception e){
+                System.out.println("Invalid");
+            }
+
+            testCases--;
+        }
+    }
+}
